368721630a3cc1049d92483140bec1648822af16
quakeN <quakendev@gmail.com>
2017-09-19 20:55:31 +0300

    Intro and basic syntax LAB - 100/100

--- IntroAndBasicSyntax/03_EmployeeData/Program.cs
+++ IntroAndBasicSyntax/03_EmployeeData/Program.cs
@@ -16,33 +16,8 @@ namespace _03_EmployeeData
 
             Console.WriteLine("Name: {0}", name);
             Console.WriteLine("Age: {0}", age);
-            switch (digits)
-            {
-                case 1:
-                    Console.WriteLine("{0}0000000{1}", eID, employeeID);
-                    break;
-                case 2:
-                    Console.WriteLine("{0}000000{1}", eID, employeeID);
-                    break;
-                case 3:
-                    Console.WriteLine("{0}00000{1}", eID, employeeID);
-                    break;
-                case 4:
-                    Console.WriteLine("{0}0000{1}", eID, employeeID);
-                    break;
-                case 5:
-                    Console.WriteLine("{0}000{1}", eID, employeeID);
-                    break;
-                case 6:
-                    Console.WriteLine("{0}00{1}", eID, employeeID);
-                    break;
-                case 7:
-                    Console.WriteLine("{0}0{1}", eID, employeeID);
-                    break;
-                case 8:
-                    Console.WriteLine("{0}{1}", eID, employeeID);
-                    break;
-            }
+            // 19/09.2017 - Now using Padleft instead of switch cases.
+            Console.WriteLine("Employee ID: {0}",employeeID.PadLeft(8,'0'));
            
             Console.WriteLine("Salary: {0:f2}", salary);
         }
