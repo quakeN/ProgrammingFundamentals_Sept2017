0084f67ba97d4c1f9746799c8fb5293998ef85b5
quakeN <quakendev@gmail.com>
2017-09-23 19:25:58 +0300

    Statements and Loops LAB - 100/100

---
+++ StatementsAndLoops/08_SumOfOddNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _08_SumOfOddNumbers
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var start = 1;
+            var sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}", start);
+                sum += start;
+                start += 2;
+            }
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
