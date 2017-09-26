e135e0a5ed4f7858a32fbbb07e0169f32fc8784c
quakeN <quakendev@gmail.com>
2017-09-26 23:12:28 +0300

    Statements and Loops EXERCISES - 100/100

---
+++ StatementsAndLoops/Exer_08_CaloriesCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exer_08_CaloriesCounter
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var calories = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine().ToLower();
+                if (product == "cheese")
+                {
+                    calories += 500;
+                }
+                else if (product == "tomato sauce")
+                {
+                    calories += 150;
+                }
+                else if (product == "salami")
+                {
+                    calories += 600;
+                }
+                else if (product == "pepper")
+                {
+                    calories += 50;
+                }
+            }
+            Console.WriteLine("Total calories: {0}", calories);
+        }
+    }
+}
