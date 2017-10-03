7ac4816a0470a451956f7ca9a46c28db9facbaab
quakeN <quakendev@gmail.com>
2017-10-03 23:46:10 +0300

    Exercises

---
+++ 06. Data types and variables/Exer_01_PracticeIntegers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exer_01_PracticeIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte a = sbyte.Parse(Console.ReadLine());
+            byte b = byte.Parse(Console.ReadLine());
+            short c = short.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            decimal e = decimal.Parse(Console.ReadLine());
+            decimal i = decimal.Parse(Console.ReadLine());
+            long k = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n" + i + "\n" + k + "\n");
+        }
+    }
