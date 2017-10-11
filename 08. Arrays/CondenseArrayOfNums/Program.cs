f62c713eb12e9de49c58ef3e261a77700c66e2c3
Georgi Danielov <quakendev@gmail.com>
2017-10-11 22:09:04 +0300

    Finished lab - 100/100

--- 08. Arrays/CondenseArrayOfNums/Program.cs
+++ 08. Arrays/CondenseArrayOfNums/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CondenseArrayOfNums
+namespace _08_CondenseArrayToNum
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var inputArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+            while (inputArr.Length > 1)
+            {
+                var condensed = new int[inputArr.Length - 1];
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = inputArr[i] + inputArr[i + 1];
+                }
+                inputArr = condensed;
+            }
+            Console.WriteLine(inputArr[0]);
         }
     }
 }
