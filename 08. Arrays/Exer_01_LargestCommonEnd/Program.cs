35014e314ed8c3cff17bec722e136740a5d01fcd
quakeN <quakendev@gmail.com>
2017-10-12 12:03:41 +0300

    Exercises

---
+++ 08. Arrays/Exer_01_LargestCommonEnd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Exer_01_LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstArr = Console.ReadLine().Split(' ').ToArray();
+            var secondArr = Console.ReadLine().Split(' ').ToArray();
+            int leftCounter = 0;
+            int rightCounter = 0;
+
+            int shorterLength = Math.Min(firstArr.Length, secondArr.Length);
+
+            for (int i = 0; i < shorterLength; i++)
+            {
+                if (firstArr[i] == secondArr[i])
+                {
+                    leftCounter++;
+                }
+                //hi php java csharp sql html css js
+                //hi php java js softuni nakov java learn
+
+                if (firstArr[firstArr.Length - 1 - i] == secondArr[secondArr.Length - 1 - i])
+                {
+                    rightCounter++;
+                }
+            }
+            Console.WriteLine(Math.Max(leftCounter, rightCounter));
+
+        }
+    }
+}
---
+++ 08. Arrays/Exer_05_CompareCharArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Exer_05_CompareCharArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstArray = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            var secondArray = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            var shorterArray = Math.Min(firstArray.Length, secondArray.Length);
+            bool equal = false;
+
+
+            for (int i = 0; i < shorterArray; i++)
+            {
+                if (firstArray[i] > secondArray[i])
+                {
+                    equal = false;
+                }
+                else
+                {
+                    equal = true;
+                }
+            }
+
+            if (equal)
+            {
+                Console.WriteLine(string.Join("", firstArray));
+                Console.WriteLine(string.Join("", secondArray));
+            }
+            else if (!equal)
+            {
+                Console.WriteLine(string.Join("", secondArray));
+                Console.WriteLine(string.Join("", firstArray));
+            }
+            else if (firstArray.Length == secondArray.Length && equal == true)
+            {
+                Console.WriteLine(string.Join("", firstArray));
+                Console.WriteLine(string.Join("", secondArray));
+            }
+        }
+    }
+}
