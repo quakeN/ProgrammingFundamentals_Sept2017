56742d99a753fa133bd84d0c9b149003559551bd
quakeN <quakendev@gmail.com>
2017-10-21 23:12:07 +0300

    Pushing Lab - 100/100

--- 11. Objects and Classes/07_SalesReport/Sale.cs
+++ 11. Objects and Classes/07_SalesReport/Sale.cs
@@ -2,7 +2,7 @@
 {
     public class Sale
     {
-        public string Town { get; set; } 
+        public string Town { get; set; }
         public string Product { get; set; }
         public decimal Price { get; set; }
         public decimal Quantity { get; set; }
