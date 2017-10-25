a5ca6e48adcc65974910be25f87fec3f6eb3d57f
quakeN <quakendev@gmail.com>
2017-10-25 23:37:08 +0300

    Making solution and pushing a problem.

---
+++ 13. Strings and Text Processing/01_ReverseString/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_ReverseString
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                var inputString = Console.ReadLine();
+                var strBuilder = new StringBuilder();
+
+                for (int i = inputString.Length - 1; i >= 0; i--)
+                {
+                    strBuilder.Append(inputString[i]);
+                }
+
+                Console.WriteLine(strBuilder.ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input string was in wrong format.");
+            }
+        }
+    }
+}
