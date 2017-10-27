5284b140e59eff1f1fd23eb42d734cca01f465aa
quakeN <quakendev@gmail.com>
2017-10-27 23:41:54 +0300

    Pushing new stuff.

---
+++ 13. Strings and Text Processing/02_CountSubStringOccur/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _02_CountSubStringOccur
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+
+            int indexer = 0;
+            int counter = 0;
+
+            while (true)
+            {
+                int found = input.IndexOf(pattern, indexer);
+
+                if (found >= 0)
+                {
+                    counter++;
+                    indexer = found + 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
