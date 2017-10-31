352ab5d8ca54117e6a8c2d29e666d31e242b88c6
quakeN <quakendev@gmail.com>
2017-10-31 22:11:54 +0200

    Pushing some solved problems.

---
+++ 14. Regular Expressions/01_MatchFullName/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_MatchFullName
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+[' '][A-Z][a-z]+\b";
+
+            MatchCollection matchedNames = Regex.Matches(input, pattern);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write($"{name} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 14. Regular Expressions/02_MatchPhoneNumber/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _02_MatchPhoneNumber
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(\b|)\+359(\s|-)2\2\d{3}\2\d{4}\b";
+
+            var numberMatches = Regex.Matches(input, pattern);
+            var validPhones = numberMatches
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(String.Join(", ", validPhones));
+        }
+    }
+}
