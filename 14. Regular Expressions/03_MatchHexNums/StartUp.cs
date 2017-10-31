e1879e4796d07c7e6f6e3fcb0dc2dbcc04a4e317
quakeN <quakendev@gmail.com>
2017-10-31 22:37:25 +0200

    Pushing more problems.

---
+++ 14. Regular Expressions/03_MatchHexNums/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_MatchHexNums
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(?:0x)?[0-9A-F]+\b";
+            var hexnumMatches = Regex.Matches(input, pattern);
+
+            var validHex = hexnumMatches
+                .Cast<Match>()
+                .Select(hex => hex.Value)
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", validHex));
+        }
+    }
+}
---
+++ 14. Regular Expressions/04_MatchDates/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _04_MatchDates
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(\d{2})(.|\/|-)([A-Z][a-z]{2})\2(\d{4})\b";
+
+            var dateMatches = Regex.Matches(input, pattern);
+
+            foreach (Match matchedDate in dateMatches)
+            {
+                var day = matchedDate.Groups[1];
+                var month = matchedDate.Groups[3];
+                var year = matchedDate.Groups[4];
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
