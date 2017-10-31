0b18e898f9cc93c3ca6b832c3bdcc74fde6debe7
quakeN <quakendev@gmail.com>
2017-11-01 00:25:16 +0200

    Pushing Lab - 100/100

---
+++ 14. Regular Expressions/05_MatchNumbers/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _05_MatchNumbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+
+            var matchedNums = Regex.Matches(input, pattern);
+
+            foreach (Match num in matchedNums)
+            {
+                Console.Write($"{num.Value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 14. Regular Expressions/06_ReplaceATag/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _06_ReplaceATag
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputHTML = Console.ReadLine();
+            var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+
+            var result = new List<string>();
+
+
+            while (inputHTML != "end")
+            {
+                bool containsValidTag = Regex.IsMatch(inputHTML, pattern);
+
+                if(!containsValidTag)
+                {
+                    result.Add(inputHTML);
+                }
+                else
+                {
+                    string replacePattern = @"[URL href=$1]$2[/URL]";
+                    string replaced = Regex.Replace(inputHTML, 
+                        pattern, replacePattern);
+                    result.Add(replaced);
+                }
+
+                inputHTML = Console.ReadLine();
+            }
+
+            foreach (var line in result)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
