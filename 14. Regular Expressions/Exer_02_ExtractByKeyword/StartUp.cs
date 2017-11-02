820a6fbae94b3d885996b764038b446ba923beae
quakeN <quakendev@gmail.com>
2017-11-02 18:25:33 +0200

    Pushing second exercise.

---
+++ 14. Regular Expressions/Exer_02_ExtractByKeyword/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace Exer_02_ExtractByKeyword
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string keyword = Console.ReadLine();
+            var text = Console.ReadLine();
+            string pattern = $@"[^.?!]*(?<=[.?\W0-9]){keyword}(?=[0-9\W.?!])[^.?!]*[.?!]";
+
+            var matches = Regex.Matches(text, pattern);
+
+            foreach (Match match in matches)
+            {
+                string output = match.Value.TrimStart(' ', '\t');
+                output = output.Remove(output.Length - 1);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
