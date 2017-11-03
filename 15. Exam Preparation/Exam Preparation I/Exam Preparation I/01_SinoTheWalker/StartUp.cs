4ae8bc57db3c93c12c28e787a1ff8ef36079ef97
quakeN <quakendev@gmail.com>
2017-11-03 21:14:41 +0200

    Creating Exam Preparation project.

---
+++ 15. Exam Preparation/Exam Preparation I/Exam Preparation I/01_SinoTheWalker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_SinoTheWalker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
