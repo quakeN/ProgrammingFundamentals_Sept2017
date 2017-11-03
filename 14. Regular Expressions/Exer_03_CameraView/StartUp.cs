a2e8773c0e676a3e4d6b2ce012956784afc77630
quakeN <quakendev@gmail.com>
2017-11-03 21:01:04 +0200

    Pushing Exercises - 100/100

---
+++ 14. Regular Expressions/Exer_03_CameraView/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace Exer_03_CameraView
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            string inputText = Console.ReadLine();
+
+            string pattern = @"\|<\w+";
+            int skipElem = numbers[0];
+            int takeElem = numbers[1];
+
+            var camerasMatches = Regex.Matches(inputText, pattern);
+            var cameras = (from Match camera in camerasMatches select camera.Value)
+                .ToArray();
+            var views = new List<String>();
+
+            foreach (string view in cameras)
+            {
+                string currentView = view;
+
+                currentView = currentView.Replace("|<", "");
+
+                var filter = currentView
+                    .ToCharArray()
+                    .Skip(skipElem)
+                    .Take(takeElem);
+
+                string viewResult = string.Join("", filter);
+                views.Add(viewResult);
+            }
+
+            Console.WriteLine(string.Join(", ", views));
+        }
+    }
+}
